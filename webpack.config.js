28a288a1767c974b89092be450f9f60245c5e18f
Shano1993 <stefan.hanotiau@gmail.com>
2022-11-14 16:43:59 +0100

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,73 +1,23 @@
 const webpack = require('webpack');
 const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
+const development = require("./webpack.config.dev");
+const production = require("./webpack.config.prod");
 
-module.exports = {
+module.exports = (env, argv) => {
 
-    entry: {
-        front: './assets/js/front/main.js',
-        admin: './assets/js/admin/main.js'
-    },
+    const config = argv.mode === 'development' ? development : production;
 
-    output: {
-        path: path.resolve(__dirname, 'public/build'),
-        filename: './js/[name]-bundle.js',
-        publicPath: '',
-    },
-
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                    },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            import: true,
-                            url: false,
-                            sourceMap: true,
-                        }
-                    },
-                ],
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: 'images/',
-                            publicPath: 'build/images/',
-                        }
-                    }
-                ]
-            },
-        ],
-    },
-
-    optimization: {
-        minimizer: [
-            new CssMinimizerPlugin(),
-        ]
-    },
-
-    plugins: [
-        new  MiniCssExtractPlugin({
-            filename: './css/[name].css',
-        }),
-        new CopyPlugin({
-            patterns: [
-                {
-                    from: 'assets/styles/images/',
-                    to: 'images/',
-                }
-            ]
-        })
-    ]
-};
+    return {
+        entry: {
+            front: './assets/js/front/main.js',
+            admin: './assets/js/admin/main.js'
+        },
+        output: {
+            path: path.resolve(__dirname, 'public/build'),
+            filename: './js/[name]-bundle.js',
+            publicPath: '/build/',
+            clean: true,
+        },
+        ...config,
+    }
+}
