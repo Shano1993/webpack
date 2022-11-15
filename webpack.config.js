d792ba8c13f9a4b689219c6fba447106fda16883
Shano1993 <stefan.hanotiau@gmail.com>
2022-11-15 13:49:30 +0100

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const webpack = require('webpack');
 const path = require('path');
-const development = require("./webpack.config.dev");
-const production = require("./webpack.config.prod");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
 
 module.exports = (env, argv) => {
 
@@ -19,5 +21,88 @@ module.exports = (env, argv) => {
             clean: true,
         },
         ...config,
+    }
+}
+
+const production = {
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            import: true,
+                            sourceMap: true,
+                        }
+                    },
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: 'images/[name][ext]'
+                }
+            },
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+                options: {
+                    presets: ['@babel/preset-env'],
+                    plugins: ['@babel/plugin-proposal-object-rest-spread']
+                }
+            }
+        ],
+    },
+
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin(),
+        ]
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: './css/[name].css',
+        }),
+    ]
+}
+
+const development = {
+    mode: 'development',
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader',
+                    {
+                        loader: "css-loader",
+                        options: {
+                            import: true,
+                            sourceMap: true,
+                        }
+                    },
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: 'images/[name][ext]'
+                }
+            },
+        ]
+    },
+    optimization: {
+        minimize: false,
     }
 }
